da64e7198047173024cf4b22a329f5551e9a00cc
Franz <fcadien1@my.centennialcollege.ca>
2020-10-07 22:02:00 -0400

    Express Generator w/ Site Structure, Routing, Navigation, and Partials

---
+++ public/Scripts/app.js
@@ -0,0 +1,12 @@
+/* Assignment1, Franz Cadiente 301098663, 10/04/2020
+ */
+// IIFE Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+})();
